e208f5479ff3e33dc9121f2658626fa68dafc164
norhan-sa <norhan.sa200@gmail.com>
2020-08-16 19:15:53 +0300

    hello

--- index.js
+++ index.js
@@ -4,13 +4,13 @@
  var    client       =     redis.createClient();
  let     port        =     process.env.PORT || 3000 ; 
 
- client.on('connect', function() {
-  console.log('connected');
- });
+//  client.on('connect', function() {
+//   console.log('connected');
+//  });
 
  app.get('/',(req,res)=>{
     res.send("Hello world");
- })
+ });
 
  app.listen(port , ()=>{
     console.log(`listening to port ${port} ... `);      
