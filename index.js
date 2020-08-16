71c1ecb73755b0605194160cf58714993f8a7469
norhan-sa <norhan.sa200@gmail.com>
2020-08-16 19:18:29 +0300

    hello

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
  const   express     =     require('express');
  const   app         =     express();
  var    redis        =     require('redis');
- var    client       =     redis.createClient();
+//  var    client       =     redis.createClient();
  let     port        =     process.env.PORT || 3000 ; 
 
 //  client.on('connect', function() {
