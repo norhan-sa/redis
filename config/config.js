5283e1522d6b2bf2cf6a1680e12a65ab44ea2e65
norhan-sa <norhan.sa200@gmail.com>
2020-08-30 18:16:46 +0300

    hello world

--- config/config.js
+++
@@ -1,9 +0,0 @@
- const fs = require('fs');
- const config = {
-    'publicKeyId': 'AKIAIQBQRP2O7WGWQOTA',               
-    'privateKey': fs.readFileSync('../private.pem'),
-    'region': 'us',                                   
-    'sandbox': true                                   
- };
-
- module.exports = config;
--- index.js
+++ index.js
@@ -1,21 +1,22 @@
  const   express     =     require('express');
  const   app         =     express();
+ const  { getDistance }     =     require('geolib');
  var    redis        =     require('redis');
-const { json } = require('express');
- let    redisport    =     process.env.PORT || 6379;
- let     port        =     process.env.PORT || 3000 ; 
- var    client       =     redis.createClient({
+ var   redisAuth     =     "uAb9OhR6ngYvLbqVe6IBl0DTuOw1EROL";
+ let      port       =     process.env.PORT || 3000 ; 
+ var     client      =     redis.createClient({
+
     port:13608,
     host:"redis-13608.c44.us-east-1-2.ec2.cloud.redislabs.com"
+
  });
- var   redisAuth     =     "uAb9OhR6ngYvLbqVe6IBl0DTuOw1EROL";
+ 
 
  client.auth(redisAuth, function(err, response){
    if(err){
    console.log(err.message);
    }
-
-   console.log("connected   :  "+response);
+   console.log("redis connection status   :  "+response);
   });
 
  app.use(express.json()); 
@@ -33,7 +34,6 @@ const { json } = require('express');
     
    client.hmset(req.body.email, { name: req.body.name , age: req.body.age , phone: req.body.phone });
    
-
    client.hgetall(req.body.email, function(err, object) {
       if(err) console.log(err.message);
       console.log(typeof object);
@@ -41,6 +41,17 @@ const { json } = require('express');
    });
 
    res.send("done");
+
+ });
+
+ app.post('/location',async (req,res)=>{
+
+    console.log( req.body.firstLocation  ,  req.body.secondLocation );
+    let first  = req.body.firstLocation;
+    let second = req.body.secondLocation;
+    let result = getDistance({latitude: first.lat ,longitude: first.long},{latitude: second.lat ,longitude: second.long} );
+    console.log(result);
+    return res.send({Distance:result});
 
  });
 
