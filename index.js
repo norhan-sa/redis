6538cce26204b0db5c2d39fb4ef13404532e99c7
norhan-sa <norhan.sa200@gmail.com>
2020-08-30 10:09:23 +0300

    hello world

--- index.js
+++ index.js
@@ -3,13 +3,19 @@
  var    redis        =     require('redis');
  let    redisport    =     process.env.PORT || 6379;
  let     port        =     process.env.PORT || 3000 ; 
- var    client       =     redis.createClient(redisport);
-
- client.on('connect', function(err , res) {
-    if(err) console.log(err.message);
-    else
-    console.log('connected');
+ var    client       =     redis.createClient({
+    port:13608,
+    host:"redis-13608.c44.us-east-1-2.ec2.cloud.redislabs.com"
  });
+ var   redisAuth     =     "uAb9OhR6ngYvLbqVe6IBl0DTuOw1EROL";
+
+ client.auth(redisAuth, function(err, response){
+   if(err){
+   console.log(err.message);
+   }
+
+   console.log("connected   :  "+response);
+  });
 
  app.get('/',(req,res)=>{
     res.send("Hello world");
