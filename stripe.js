360a2d9f347efc1f47328d9d181ca091d14dc042
norhan-sa <norhan.sa200@gmail.com>
2020-12-09 23:29:19 +0200

    ubuntu commit

--- stripe.js
+++ stripe.js
@@ -23,17 +23,34 @@ require('dotenv').config();
       total += element.price * element.quantity;             
     });
 
-    stripe.charges.create({
-       amount: total,
-       source: req.body.stripeTokenID,
-       currency: 'usd'      
+    stripe.customers.create({
+          email: 'test@gmail.com',    
+          source: req.body.stripeToken      
+    }).then(customer =>{
+       stripe.charges.create({
+          amount: total,
+          source: req.body.stripeTokenID,
+          currency: 'usd'      
+       });
     }).then(()=>{
-       console.log('charged successfully');  
-       res.send('successfully purchased');     
-    }).catch((err)=>{
-       console.error(err);
-       res.status(500).end();
+          console.log('charged successfully');  
+          res.send('successfully purchased');
+    }).catch(err=>{
+          console.error(err);
+          res.status(500).send({msg:'err'});
     });
+
+//     stripe.charges.create({
+//        amount: total,
+//        source: req.body.stripeTokenID,
+//        currency: 'usd'      
+//     }).then(()=>{
+//        console.log('charged successfully');  
+//        res.send('successfully purchased');     
+//     }).catch((err)=>{
+//        console.error(err);
+//        res.status(500).end();
+//     });
  });
 
 
